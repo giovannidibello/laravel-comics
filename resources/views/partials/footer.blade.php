013c00645d302dcfe4184d508ea1b523c5a1193c
Giovanni <dibellogio@gmail.com>
2025-05-12 15:37:56 +0200

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Footer</title>
+</head>
+<body>
+    <footer>
+
+        <div class="container">
+
+            <div class="footer-sections">
+
+                <div class="side-left">
+
+                    <div class="footer-column">
+                        <ul>
+                            <li><h6>DC COMICS</h6></li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+
+
+                        <ul>
+                            <li><h6>SHOP</h6></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+
+                    </div>
+
+
+                    <div class="footer-column">
+
+                        <ul>
+                            <li><h6>DC</h6></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer-column">
+
+                        <ul>
+                            <li><h6>SITES</h6></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+                <div class="side-right">
+
+                </div>
+
+            </div>
+        </div>
+
+
+
+        <div class="footer-bottom">
+
+            <div class="container">
+                <button>SIGN-UP NOW!</button>
+                <div class="social-links">
+                    <span>FOLLOW US</span>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook" /></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter" /></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube" /></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest" /></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope" /></a>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+
+</body>
+</html>
