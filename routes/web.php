ef97e6949c301eacc252402ed2cea5d0d8828d6c
Giovanni <dibellogio@gmail.com>
2025-05-12 15:38:06 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name("home");
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name("characters");
+
+Route::get('/comics', function () {
+    return view('comics');
+})->name("comics");
