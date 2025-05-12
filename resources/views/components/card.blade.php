57a2949a8a21d5a56dd96fba9652ac1d7ea408a8
Giovanni <dibellogio@gmail.com>
2025-05-12 15:38:27 +0200

    add component card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+<div class="prod-card">
+
+    <img src="{{ $thumb }}" alt="{{$titolo}}" class="card-img-top">
+
+    <div class="card-title">
+        <h5>{{$titolo}}</h5>
+        
+    </div>
+   
+</div>
